8b96be9a46736415cd27845ca6d95894b2633b55
toxiapo <toxiapo@gmail.com>
2024-06-17 13:18:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Running this example
+
+```bash
+npm i
+npm run build -w app1
+npm run ios -w app1
+```
---
+++ apps/app1/src/js/capacitor-welcome.js
@@ -0,0 +1,193 @@
+import { SplashScreen } from "@capacitor/splash-screen";
+import { Camera } from "@capacitor/camera";
+import {
+  NewRelicCapacitorPlugin,
+  NREnums,
+} from "@newrelic/newrelic-capacitor-plugin";
+
+const appkey = "";
+
+const agentConfig = {
+  // Android specific option
+  // Optional: Enable or disable collection of event data.
+  analyticsEventEnabled: true,
+
+  // iOS specific option
+  // Optional: Enable/Disable automatic instrumentation of WebViews.
+  webViewInstrumentation: true,
+
+  // Optional: Enable or disable crash reporting.
+  crashReportingEnabled: true,
+
+  // Optional: Enable or disable interaction tracing. Trace instrumentation still occurs, but no traces are harvested. This will disable default and custom interactions.
+  interactionTracingEnabled: true,
+
+  // Optional: Enable or disable reporting successful HTTP requests to the MobileRequest event type.
+  networkRequestEnabled: true,
+
+  // Optional: Enable or disable reporting network and HTTP request errors to the MobileRequestError event type.
+  networkErrorRequestEnabled: true,
+
+  // Optional: Enable or disable capture of HTTP response bodies for HTTP error traces, and MobileRequestError events.
+  httpResponseBodyCaptureEnabled: true,
+
+  // Optional: Enable or disable agent logging.
+  loggingEnabled: true,
+
+  // Optional: Specifies the log level. Omit this field for the default log level.
+  // Options include: ERROR (least verbose), WARNING, INFO, VERBOSE, AUDIT (most verbose).
+  logLevel: NREnums.LogLevel.INFO,
+
+  // Optional: Enable or disable sending JS console logs to New Relic.
+  sendConsoleEvents: false,
+
+  // Optional:Enable or disable reporting data using different endpoints for US government clients
+  fedRampEnabled: false,
+};
+
+NewRelicCapacitorPlugin.start({
+  appKey: appKey,
+  agentConfiguration: agentConfig,
+});
+
+window.customElements.define(
+  "capacitor-welcome",
+  class extends HTMLElement {
+    constructor() {
+      super();
+
+      SplashScreen.hide();
+
+      const root = this.attachShadow({ mode: "open" });
+
+      root.innerHTML = `
+    <style>
+      :host {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        display: block;
+        width: 100%;
+        height: 100%;
+      }
+      h1, h2, h3, h4, h5 {
+        text-transform: uppercase;
+      }
+      .button {
+        display: inline-block;
+        padding: 10px;
+        background-color: #73B5F6;
+        color: #fff;
+        font-size: 0.9em;
+        border: 0;
+        border-radius: 3px;
+        text-decoration: none;
+        cursor: pointer;
+      }
+      main {
+        padding: 15px;
+      }
+      main hr { height: 1px; background-color: #eee; border: 0; }
+      main h1 {
+        font-size: 1.4em;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+      }
+      main h2 {
+        font-size: 1.1em;
+      }
+      main h3 {
+        font-size: 0.9em;
+      }
+      main p {
+        color: #333;
+      }
+      main pre {
+        white-space: pre-line;
+      }
+    </style>
+    <div>
+      <capacitor-welcome-titlebar>
+        <h1>Capacitor</h1>
+      </capacitor-welcome-titlebar>
+      <main>
+        <p>
+          Capacitor makes it easy to build powerful apps for the app stores, mobile web (Progressive Web Apps), and desktop, all
+          with a single code base.
+        </p>
+        <h2>Getting Started</h2>
+        <p>
+          You'll probably need a UI framework to build a full-featured app. Might we recommend
+          <a target="_blank" href="http://ionicframework.com/">Ionic</a>?
+        </p>
+        <p>
+          Visit <a href="https://capacitorjs.com">capacitorjs.com</a> for information
+          on using native features, building plugins, and more.
+        </p>
+        <a href="https://capacitorjs.com" target="_blank" class="button">Read more</a>
+        <h2>Tiny Demo</h2>
+        <p>
+          This demo shows how to call Capacitor plugins. Say cheese!
+        </p>
+        <p>
+          <button class="button" id="take-photo">Take Photo</button>
+        </p>
+        <p>
+          <img id="image" style="max-width: 100%">
+        </p>
+      </main>
+    </div>
+    `;
+    }
+
+    connectedCallback() {
+      const self = this;
+
+      self.shadowRoot
+        .querySelector("#take-photo")
+        .addEventListener("click", async function (e) {
+          try {
+            const photo = await Camera.getPhoto({
+              resultType: "uri",
+            });
+
+            const image = self.shadowRoot.querySelector("#image");
+            if (!image) {
+              return;
+            }
+
+            image.src = photo.webPath;
+          } catch (e) {
+            console.warn("User cancelled", e);
+          }
+        });
+    }
+  }
+);
+
+window.customElements.define(
+  "capacitor-welcome-titlebar",
+  class extends HTMLElement {
+    constructor() {
+      super();
+      const root = this.attachShadow({ mode: "open" });
+      root.innerHTML = `
+    <style>
+      :host {
+        position: relative;
+        display: block;
+        padding: 15px 15px 15px 15px;
+        text-align: center;
+        background-color: #73B5F6;
+      }
+      ::slotted(h1) {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        font-size: 0.9em;
+        font-weight: 600;
+        color: #fff;
+      }
+    </style>
+    <slot></slot>
+    `;
+    }
+  }
+);
